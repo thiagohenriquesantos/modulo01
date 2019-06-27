438c7ec9282b83541ec85c9c386d67cc7aca4ff0
thiagohenriquesantos <thiagosantosengcivil@gmail.com>
2019-06-27 15:33:46 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const server = express();
+
+// Query params = ?teste=1
+// Route params = /users/1
+// Request body = { "name": "Thiago", "email": "thiagosantosdev@gmail.com" }
+
+const users = ["Diego", "Claudio", "Victor"];
+
+server.get("/users/:index", (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
+});
+
+server.listen(3000);
